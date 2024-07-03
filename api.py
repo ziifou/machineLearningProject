1eec924a57aed1faa4cbe4022d7b87fac1bf652c
ziifou <khelafi.sifeddine@gmail.com>
2024-07-03 22:22:54 +0200

    add documentation for the api

--- api.py
+++ api.py
@@ -4,7 +4,8 @@ import joblib
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from openai import OpenAI
-client = OpenAI("")
+
+client = OpenAI()
 
 
 class Message(BaseModel):
@@ -28,8 +29,14 @@ class TrainData(BaseModel):
 class PredictData(BaseModel):
     data: list
 
-@app.post("/training", tags=["Model Training"], summary="Entraîner un modèle")
+@app.post("/training", tags=["Model Training"], summary="Entraîner un modèle", description="Entraîne un modèle de régression logistique avec les données fournies.")
 def train_model(train_data: TrainData):
+    """
+    Entraîne un modèle de régression logistique avec les données fournies.
+
+    - **data**: Liste des caractéristiques pour l'entraînement.
+    - **target**: Liste des étiquettes cibles.
+    """
     try:
         print(train_data.data)
         X = pd.DataFrame(train_data.data)
@@ -41,8 +48,13 @@ def train_model(train_data: TrainData):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@app.post("/predict", tags=["Model Prediction"], summary="Faire une prédiction")
+@app.post("/predict", tags=["Model Prediction"], summary="Faire une prédiction", description="Prédit la classe (Malade ou Sain) pour les données fournies.")
 def predict(data: PredictData):
+    """
+    Prédit la classe (Malade ou Sain) pour les données fournies.
+
+    - **data**: Liste des caractéristiques pour la prédiction.
+    """
     try:
         model = joblib.load('./model.pkl')
         X = pd.DataFrame(data.data)
@@ -62,13 +74,20 @@ class ChatResponse(BaseModel):
     response: str
 
 
-@app.post("/gpt-response")
+@app.post("/gpt-response", response_model=ChatResponse, tags=["Chat with GPT"], summary="Obtenir une réponse GPT", description="Obtenez une réponse du modèle GPT-3.5-turbo pour une question donnée.")
 async def chat(request: ChatRequest):
-    print(request)
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": request.message},
-    ]
-    )
-    return {"response": completion.choices[0].message}
+    """
+    Obtenez une réponse du modèle GPT-3.5-turbo pour une question donnée.
+
+    - **message**: La question à poser au modèle GPT.
+    """
+    try:
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": request.message},
+            ]
+        )
+        return {"response": completion.choices[0].message}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
